117d9e4d4fed40b87c1187ee07b33c8a6b766639
Afeef <afeef-qerem@hotmail.com>
2025-12-30 13:58:03 +0200

    Automatic commit: 2025-12-30 13:58:03

--- html/index.php
+++ html/index.php
@@ -1,6 +1,6 @@
 <?php
 //making this comment to test if the automation works
-$servername = "db"; // MySQL container name name inside docker-compose
+$servername = "db"; // MySQL container name name inside 
 $username = "root";
 $password = "mypassword";
 $dbname = "restaurant_db";
